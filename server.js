75d8572112d84e3ef851541f4ed868ace77848d1
Trần Nhất Quang <trannhatquang2405@gmail.com>
2023-10-07 20:04:09 +0700

    finish

--- server.js
+++ server.js
@@ -1,45 +1,38 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util.js';
+import { filterImageFromURL, deleteLocalFiles } from './util/util.js';
 
 
 
-  // Init the Express application
-  const app = express();
+// Init the Express application
+const app = express();
 
-  // Set the network port
-  const port = process.env.PORT || 8082;
-  
-  // Use the body parser middleware for post requests
-  app.use(bodyParser.json());
+// Set the network port
+const port = process.env.PORT || 8082;
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+// Use the body parser middleware for post requests
+app.use(bodyParser.json());
 
-    /**************************************************************************** */
+app.get("/filteredimage", async (req, res) => {
+  const { image_url: imageUrl } = req.query;
+  if (!imageUrl || !isUri(imageUrl)) {
+    return res.status(400).send({ auth: false, message: 'Image url is missing or malformed' });
+  }
 
-  //! END @TODO1
-  
-  // Root Endpoint
-  // Displays a simple message to the user
-  app.get( "/", async (req, res) => {
-    res.send("try GET /filteredimage?image_url={{}}")
-  } );
-  
+  const filteredPath = await filterImageFromURL(imageUrl);
 
-  // Start the Server
-  app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
+  res.sendFile(filteredPath, {}, () => deleteLocalFiles([filteredPath]));
+});
+
+// Root Endpoint
+// Displays a simple message to the user
+app.get("/", async (req, res) => {
+  res.send("try GET /filteredimage?image_url={{}}")
+});
+
+
+// Start the Server
+app.listen(port, () => {
+  console.log(`server running http://localhost:${port}`);
+  console.log(`press CTRL+C to stop server`);
+});
